38d96b645b24c31fc0731657aed67d1728a36ed2
popshk <dmitry.popkov.1994@gmail.com>
2019-04-05 17:49:53 +0300

    add something user pages

--- src/main/java/com/example/popshk/domain/Role.java
+++ src/main/java/com/example/popshk/domain/Role.java
@@ -3,7 +3,7 @@ package com.example.popshk.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER,ADMIN;
 
     @Override
     public String getAuthority() {
