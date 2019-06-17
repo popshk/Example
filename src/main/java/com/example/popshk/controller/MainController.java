e770f542b4a822eb6fe7d57dcf0c9ecd446877c0
popshk <dmitry.popkov.1994@gmail.com>
2019-06-17 21:39:25 +0300

    upload img to server

--- src/main/java/com/example/popshk/controller/MainController.java
+++ src/main/java/com/example/popshk/controller/MainController.java
@@ -4,19 +4,28 @@ import com.example.popshk.domain.Message;
 import com.example.popshk.domain.User;
 import com.example.popshk.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
 
     @Autowired
     private MessageRepository messageRepository;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting (Map<String,Object> model){
@@ -33,7 +42,9 @@ public class MainController {
         }else {
             messages = messageRepository.findAll();
         }
+//
 
+        //
         model.addAttribute("messages",messages);
         model.addAttribute("filter",filter);
 
@@ -41,8 +52,29 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@AuthenticationPrincipal User user,@RequestParam String text, @RequestParam String tag, Map<String,Object> model){
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String,Object> model,
+            @RequestParam("file") MultipartFile file
+    ) throws IOException {
         Message message = new Message(text, tag, user);
+
+        if (file !=null) {
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists()){
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/"+resultFilename));
+
+            message.setFilename(resultFilename);
+        }
 
         messageRepository.save(message);
 
@@ -50,6 +82,8 @@ public class MainController {
 
         model.put("messages",messages);
 
-        return "main";
+        /*use this redirect, because if you update this page in browser,
+        form POST data all from previous one!!!*/
+        return "redirect:/main";
     }
 }
